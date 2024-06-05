4ee46ee2b125817ff3406e025985415deb5686d0
kojo Adu-Gyamfi <adugyamfikonadu48@gmail.com>
2024-06-05 06:00:13 -0500

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Kojo Adu-Gyamfi on 6/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
